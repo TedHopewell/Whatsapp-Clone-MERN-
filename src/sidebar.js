95c5f3da82f2434a5a52f3db24c66e3d4fce86c8
TedHopewell <hopewellmaome@gmail.com>
2022-12-20 22:28:28 +0200

    search UI in progress

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import '../src/sidebar.css'
-import { ChatOutlined, DonutLarge, MoreVert } from '@mui/icons-material'
+import { ChatOutlined, DonutLarge, MoreVert, SearchOutlined } from '@mui/icons-material'
 import {Avatar, IconButton } from '@mui/material'
 
 function Sidebar() {
   return (
     <div className='sidebar'>
         <div className='sidebar_header'>
-            <Avatar source='https://media.licdn.com/media/AAYQAQSOAAgAAQAAAAAAAB-zrMZEDXI2T62PSuT6kpB6qg.png' />
+            <Avatar src='https://e3.365dm.com/22/12/2048x1152/skynews-messi-instagram_6002141.png?20221220141358' />
             <div className='sidebar_headerRight'>
                 <IconButton>
                         <DonutLarge />
@@ -21,7 +21,13 @@ function Sidebar() {
                 </IconButton>
             </div>
         </div>
-        {/* <h1>I'm the sidebar</h1> */}
+        <div className='sidebar_search'>
+            <div className='sidebar_searchContainer'>
+                <SearchOutlined />
+                <input placeholder='Search or start a new chat' type="text" />
+            </div>
+        </div>
+        
     </div>
   )
 }
