aac66eed00300a939541304ff6ed00d8f74d2a4f
TedHopewell <hopewellmaome@gmail.com>
2022-12-22 06:32:20 +0200

    chat footer CSS/UI complete

--- src/chat.js
+++ src/chat.js
@@ -1,4 +1,4 @@
-import { AttachFile, MoreVert, SearchOutlined } from '@mui/icons-material'
+import { AttachFile, InsertEmoticon, Mic, MoreVert, SearchOutlined } from '@mui/icons-material'
 import { Avatar, IconButton } from '@mui/material'
 import React from 'react'
 import '../src/chat.css'
@@ -9,7 +9,7 @@ function Chat() {
     <div className='chat'>
         <div className='chat_header'>
             <Avatar />
-            <div className='chat_hearInfo'>
+            <div className='chat_headerInfo'>
                 <h3>Jabu</h3>
                 <p>Last seen at...</p>
             </div>
@@ -26,6 +26,32 @@ function Chat() {
                 </IconButton>
 
             </div>
+        </div>
+
+        <div className='chat_body'>
+            <p className='chat_message'>
+                <span className='chat_name'>Hopewell</span>
+                Hi there we are building whatsapp you and I
+                <span className='chat_timestamp'>{new Date().toUTCString()}</span>
+            </p>
+
+            <p className='chat message chat_receiver'>
+                <span className='chat_name'>Hopewell</span>
+                Hi there we are building whatsapp you and I
+                <span className='chat_timestamp'>{new Date().toUTCString()}</span>
+            </p>
+        </div>
+
+        <div className='chat_footer'>
+            <InsertEmoticon />
+            <form>
+                <input placeholder='Type a message'
+                    type="text" />
+                <button type='submit'>
+                    Send a message
+                </button>
+            </form>
+            <Mic />
         </div>
     </div>
   )
