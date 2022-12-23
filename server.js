7f4652d8e861437a7f8746ba70a3b31bdb64cb48
TedHopewell <hopewellmaome@gmail.com>
2022-12-23 06:39:21 +0200

    Express script test working, mongoDB working backend is still in progress

---
+++ server.js
@@ -0,0 +1,17 @@
+/////Here in this page server.js I am creating an API
+
+//imports
+import express from 'express'
+// App configuration
+const app = express()
+const port = process.env.PORT || 9000
+// middlewear
+
+// DB config
+
+// ???
+
+// api routes
+app.get('/',(req,res)=>res.status(200).send('What the fuck bro Im on another level with this shit'))
+// Listen
+app.listen(port, () => console.log(`Listening on localhost:${port}`));
